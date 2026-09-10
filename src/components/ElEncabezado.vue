d8928f7d8cf914a0dbd50507929fb4299e5c763e
AlejandraFranSi <=>
2026-09-09 18:03:46 -0600

    Ajustando el header

--- src/components/ElEncabezado.vue
+++ src/components/ElEncabezado.vue
@@ -7,10 +7,18 @@ const dataStore = useDataStore()
   <div class="contenedor-header flex">
     <h3><IconoEstadisticas /> Estadísticas PNDA</h3>
     <div class="secciones flex">
-      <button aria-label="Ir a nosotros" @click="dataStore.actualizarSeccion('nosotros')">
+      <button
+        aria-label="Ir a nosotros"
+        :class="dataStore.seccion === 'nosotros' ? 'selected' : 'no-selected'"
+        @click="dataStore.actualizarSeccion('nosotros')"
+      >
         Nosotros
       </button>
-      <button aria-label="Ir a instituciones" @click="dataStore.actualizarSeccion('instituciones')">
+      <button
+        aria-label="Ir a instituciones"
+        :class="dataStore.seccion === 'instituciones' ? 'selected' : 'no-selected'"
+        @click="dataStore.actualizarSeccion('instituciones')"
+      >
         Instituciones
       </button>
     </div>
@@ -18,17 +26,51 @@ const dataStore = useDataStore()
 </template>
 <style scoped>
 .contenedor-header {
-  padding: 0px 16px;
+  padding: 0px 8px;
   max-height: 80px;
-  background-color: var(--color-secundario-10);
+  background-color: var(--color-secundario-11);
   color: var(--color-neutro-0);
   align-items: center;
   gap: 16px;
 }
 .secciones {
-  justify-self: flex-end;
   gap: 8px;
   position: absolute;
-  right: 100px;
+  right: 16px;
+  height: 80px;
+}
+
+button {
+  height: 100%;
+  background-color: var(--color-secundario-11);
+  color: var(--color-neutro-0);
+  border-radius: 0px;
+}
+
+button:hover {
+  background-color: var(--color-secundario-7);
+}
+
+.selected {
+  border-bottom: solid 4px var(--color-secundario-6);
+}
+
+@media (max-width: 580px) {
+  .contenedor-header {
+    margin-bottom: 40px;
+  }
+  .secciones {
+    right: 0px;
+    top: 80px;
+    height: 40px;
+    width: 100vw;
+    padding: 0px 8px;
+    gap: 0px;
+    background-color: var(--color-secundario-11);
+    border-top: solid 1px var(--color-secundario-6);
+  }
+  button {
+    width: 50%;
+  }
 }
 </style>
